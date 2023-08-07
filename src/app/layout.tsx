cd0a2f6752363d554c7cb850cb1c0b7bba3f3cd0
cavera <cavera.de@gmail.com>
2023-08-07 17:42:02 -0500

    openGraph corrected (testing favicon)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export const metadata: Metadata = {
 			{ url: 'favicon-32x32.png', sizes: '32x32' },
 			{ url: 'favicon-16x16.png', sizes: '16x16' },
 		],
-		shortcut: 'icon.png',
+		shortcut: 'icon.ico',
 		apple: 'apple-touch-icon.png',
 		other: {
 			rel: 'apple-touch-icon-precomposed',
