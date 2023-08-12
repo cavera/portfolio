c35bb605be09f89ab7d91b7d10ea5654596070ae
cavera <cavera.de@gmail.com>
2023-08-11 20:08:46 -0500

    gsap animation of header bg

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,11 +1,42 @@
+'use client'
+import { use, useLayoutEffect, useRef } from 'react'
 import { HeaderLogo } from './HeaderLogo'
 import { Menu } from './Menu'
 
 import styles from './TopBar.module.scss'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
 export const TopBar = () => {
+	const topBarBgRef = useRef<HTMLDivElement>(null)
+	useLayoutEffect(() => {
+		gsap.registerPlugin(ScrollTrigger)
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: document.documentElement,
+				start: 'top top',
+				end: '+=100',
+				scrub: true,
+				markers: false,
+			},
+			defaults: {},
+		})
+
+		tl.fromTo(
+			topBarBgRef.current,
+			{
+				autoAlpha: 0,
+			},
+			{
+				autoAlpha: 1,
+			}
+		)
+	}, [])
 	return (
 		<header className={styles.header}>
+			<div
+				className={styles.header_bg}
+				ref={topBarBgRef}></div>
 			<HeaderLogo />
 			<Menu />
 		</header>
