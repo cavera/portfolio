c15db3a269b6fe0f4caf7093f8f806414970e0a0
cavera <cavera.de@gmail.com>
2023-08-22 17:17:00 -0500

    improved footer text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,18 @@ export const Footer = () => {
 		<footer className={styles.footer}>
 			<article className={styles.footer_info}>
 				<p>cavera 2023</p>
-				<p className={styles.made_with}>Made with Next.js, TypeScript and GSAP</p>
+				<p className={styles.made_with}>
+					Made with Next.js, TypeScript and GSAP. {'Uses '}
+					{
+						<a
+							href='https://notion.dev/'
+							target='_blank'
+							className={styles.footer_link}>
+							Notion
+						</a>
+					}
+					{' as CMS.'}
+				</p>
 			</article>
 
 			<div className={styles.social}>
