d5ad4ffdddf9e40636e36501bcb85d3afa593468
cavera <cavera.de@gmail.com>
2023-08-22 13:05:11 -0500

    improved footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,10 @@ import styles from './Footer.module.scss'
 export const Footer = () => {
 	return (
 		<footer className={styles.footer}>
-			<span className={styles.footer_info}>cavera 2023</span>
+			<article className={styles.footer_info}>
+				<p>cavera 2023</p>
+				<p className={styles.made_with}>Made with Next.js, TypeScript and GSAP</p>
+			</article>
 
 			<div className={styles.social}>
 				<SocialBar size={24} />
